b2c7e19a249e3b0bad3fbc69104cf23afeb0a96d
Pelle <pellebranden@me.com>
2015-10-26 18:15:38 +0100

    Added delay to every call for testing the web

--- src/Quilt4.Api/Startup.cs
+++ src/Quilt4.Api/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
@@ -47,6 +48,19 @@ namespace Quilt4.Api
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            //Adding delay to every call
+            //For testing loading-panel on the web
+            app.Use(next => async context =>
+            {
+                var rand = new Random(1).Next(5);
+                Console.WriteLine(rand);
+                Thread.Sleep(1000 * rand);
+
+                await next.Invoke(context);
+                
+            });
+
 
             // Configure the HTTP request pipeline.
             app.UseStaticFiles();

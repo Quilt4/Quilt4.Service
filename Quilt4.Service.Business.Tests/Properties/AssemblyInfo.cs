56696edb3cf7aa84978701de8b9fcad4d69a2d60
poxet <poxet@clowncrew.com>
2016-10-26 23:35:33 +0200

    Updated version information file

--- Quilt4.Service.Business.Tests/Properties/AssemblyInfo.cs
+++ Quilt4.Service.Business.Tests/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Quilt4.Service.Business.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("Thargelion AB")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Quilt4.Service.Business.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("The MIT License (MIT) Copyright © Thargelion AB 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c13a6b39-fac8-49fb-b1b2-8ac5ddfcfa1b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

fed8025485b9497371a198a1e33b7ad4c2ae72c8
poxet <poxet@clowncrew.com>
2017-03-19 13:50:18 +0100

    create user area

---
+++ Quilt4.Service/Areas/User/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Quilt4.Service.Converters;
+using Quilt4.Service.Interface.Business;
+
+namespace Quilt4.Service.Areas.User.Controllers
+{
+    [Authorize]
+    public class ProjectController : Controller
+    {
+        private readonly IProjectBusiness _projectBusiness;
+
+        public ProjectController(IProjectBusiness projectBusiness)
+        {
+            _projectBusiness = projectBusiness;
+        }
+
+        public ActionResult Index()
+        {
+            var projects = _projectBusiness.GetProjects(User.Identity.Name);
+            var model = projects.Select(x => x.ToProjectData());
+            return View(model);
+        }
+    }
+}

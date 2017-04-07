9fd261eb40fc3379f2836c8c07dbc9bf1fa485aa
Daniel Bohlin <poxet@clowncrew.com>
2017-04-07 17:00:01 +0200

    wip

--- Quilt4.Service.Business/Class1.cs
+++ Quilt4.Service.Business/Class1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using Microsoft.Owin.Cors;
 using Microsoft.Owin.Hosting;
 using Owin;
+using Quilt4Net.Core.DataTransfer;
 
 namespace Quilt4.Service.Business
 {
@@ -46,26 +49,102 @@ namespace Quilt4.Service.Business
                 // since this branch is already runs under the "/signalr"
                 // path.
                 map.RunSignalR(hubConfiguration);
+
+                Console.WriteLine("mapped");
             });
+
+            Console.WriteLine("configured");
         }
+    }
+
+    //public interface ICommand
+    //{
+    //}
+
+    //public abstract class BaseCommandHandler<TCommand> : IHandleCommand<TCommand> where TCommand : ICommand
+    //{
+    //    public ISessionFactory SessionFactory { get; set; }
+    //    public IConnectionManager ConnectionManager { get; set; }
+
+    //    protected ISession Session
+    //    {
+    //        get { return SessionFactory.GetCurrentSession(); }
+    //    }
+
+    //    #region Implementation of IHandleCommand<TCommand>
+    //    public abstract void Handle(TCommand command);
+    //    #endregion
+    //}
+
+    public class CommandDto
+    {
+        public Guid CommandKey { get; set; }
+        public string Name { get; set; }
+        public object Data { get; set; }
     }
 
     [HubName("MyHub")]
     public class MyHub : Hub
     {
-        public string Send(string message)
+        private readonly BlockingCollection<CommandDto> _commands = new BlockingCollection<CommandDto>();
+
+        public MyHub()
         {
-            Console.WriteLine("Recieved Send function with payload '" + message + "'.");
-            Clients.All.addMessage("Echo " + message);
-            return "Yeah!";
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var item = _commands.Take();
+                    var type = Type.GetType($"Quilt4.Service.Business.{item.Name}Handler, Quilt4.Service.Business");
+                    if(type == null) throw new InvalidOperationException($"Cannot find {item.Name}Handler class. Unable to handle command");
+                    var obj = Activator.CreateInstance(type);
+                    var m = obj.GetType().GetMethod("Execute");
+
+                    var data = new object();
+                    //var dataType = Type.GetType(item.Name);
+                    //var payload = Activator.CreateInstance(dataType);
+
+                    m.Invoke(obj, new[] { data });
+                }
+            });
         }
 
-        //public void DoSomething(string message)
-        //{
-        //    Console.WriteLine("DoSomething...");
-        //    //Clients.All.addMessage("Echo " + message);
+        public override Task OnConnected()
+        {
+            Console.WriteLine("OnConnected");
+            return base.OnConnected();
+        }
 
-        //    //Groups.Add(Context.ConnectionId, groupName);
-        //}
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Console.WriteLine("OnDisconnected");
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public Guid Execute(CommandDto command)
+        {
+            //TODO: Persist this command in a event source database. So that it can be replayed later
+            _commands.Add(command);
+            return command.CommandKey;
+        }
+    }
+
+    public abstract class HandlerBase<T>
+    {
+        public abstract void Execute(T command);
+    }
+
+    public class IssueRequestHandler : HandlerBase<IssueRequest>
+    {
+        public override void Execute(IssueRequest command)
+        {
+        }
+    }
+
+    public class SessionRequestHandler : HandlerBase<SessionRequest>
+    {
+        public override void Execute(SessionRequest command)
+        {
+        }
     }
 }
